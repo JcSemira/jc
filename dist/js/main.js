dccb1ddf642b0a03935c0214170d4cf396a60f7e
cr50.john <cr50.john@enfrausaph.com>
2018-08-07 01:32:15 +0800

    Main Sass

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,15 +16,15 @@ function toggleMenu(){
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        navItems.forEach(item=> item.classList.add('show0'));
-    // Select Menu State
+        navItems.forEach(item=> item.classList.add('show'));
+    // Set Menu State
     showMenu = true;
     } else{
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItems.forEach(item=> item.classList.remove('show0'));
+        navItems.forEach(item=> item.classList.remove('show'));
     //Set Menu State
     showMenu=false;
     }
